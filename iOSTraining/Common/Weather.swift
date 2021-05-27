9e47b4cbc4e749526a5d76093164f5c06738921a
Shota Fuchikami <0904kinazu@gmail.com>
2021-05-27 22:37:33 +0900

    feat: display wreather image

---
+++ iOSTraining/Common/Weather.swift
@@ -0,0 +1,13 @@
+//
+//  Weather.swift
+//  iOSTraining
+//
+//  Created by Shota Fuchikami on 2021/05/27.
+//
+
+/// 表示する天候パターン
+public enum Weahter: String {
+    case sunny = "sunny"
+    case cloudy = "cloudy"
+    case rainy = "rainy"
+}
